b80d4d4ba165cbdf9516f22acbe3dac6afef2032
najmunoishi <najmunnaharoishi121@gmail.com>
2024-02-04 20:42:53 +0600

    drawer design done

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       home: DrawerScreen(),
     );
   }
